978101e1f7d5ae1c6f7ec5bfc766877b4fb4d08c
Nils Druyen <nils.druyen@fressnapf.com>
2022-02-12 23:27:10 +0100

    Add keyframe animations

--- app/src/main/java/de/nilsdruyen/composeparty/paths/PawIcon.kt
+++ app/src/main/java/de/nilsdruyen/composeparty/paths/PawIcon.kt
@@ -1,15 +1,15 @@
 package de.nilsdruyen.composeparty.paths
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +27,9 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun PawIcon(modifier: Modifier = Modifier) {
+    val color = SolidColor(Color.White)
+    val duration = 1000
+
     val parts = listOf(
         "M-0.88 -4.85 C-11.24,-1.32 -9.99,5.65 -11.9,12.35 C-12.79,15.43 -24.91,25.57 -17.64,35.27 C-11.13,43.95 4.34,33.27 8.82,32.85 C13.45,32.41 28,38.8 33.51,26.45 C38.45,15.38 24.69,11.9 19.4,7.72 C14.11,3.53 12.59,-9.43 -0.88,-4.85c",
         "M-43.25 -8 C-45.75,-5 -46,4.5 -41.75,10.25 C-37.5,16 -30.75,19.75 -23.5,17.75 C-16.25,15.75 -16.25,5.25 -22.75,-2.75 C-29.79,-11.41 -38.7,-13.46 -43.25,-8c",
@@ -36,17 +39,57 @@ fun PawIcon(modifier: Modifier = Modifier) {
     )
 
     val drawParts = remember { parts.map { addPathNodes(it) } }
-    val animatedProgress = remember { Animatable(0f) }
 
-    LaunchedEffect(Unit) {
-        animatedProgress.animateTo(
-            targetValue = 1f,
-            animationSpec = infiniteRepeatable(
-                animation = tween(durationMillis = 600, easing = FastOutLinearInEasing),
-                repeatMode = RepeatMode.Reverse,
-            ),
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val paw01 by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = duration
+                0.0f at 0
+                1f at 300 with FastOutSlowInEasing
+            },
+            repeatMode = RepeatMode.Reverse
         )
-    }
+    )
+    val paw02 by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = duration
+                0.0f at 200
+                1f at 500 with FastOutSlowInEasing
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val paw03 by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = duration
+                0.0f at 400
+                1f at 700 with FastOutSlowInEasing
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val paw04 by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = duration
+                0.0f at 600
+                1f at 900 with FastOutSlowInEasing
+            },
+            repeatMode = RepeatMode.Reverse
+        )
+    )
 
     Image(
         modifier = Modifier
@@ -66,64 +109,64 @@ fun PawIcon(modifier: Modifier = Modifier) {
                 ) {
                     Path(
                         pathData = drawParts[0],
-                        stroke = SolidColor(Color.White),
+                        stroke = color,
                         strokeLineWidth = 4f
                     )
                 }
                 Group(
-                    name = "02",
-                    scaleX = 0.959f * animatedProgress.value,
-                    scaleY = 1f * animatedProgress.value,
-                    translationX = 49.278f,
-                    translationY = 50.879f,
+                    name = "paw01",
+                    scaleX = 0.959f * paw01,
+                    scaleY = 1f * paw01,
+                    translationX = 49.278f - 25 * (1 - paw01),
+                    translationY = 50.879f - 5 * (1 - paw01),
                 ) {
                     Path(
                         pathData = drawParts[1],
-                        stroke = SolidColor(Color.White),
+                        stroke = color,
                         strokeLineWidth = 4f,
                     )
                 }
                 Group(
-                    name = "03",
-                    scaleX = 0.94136f * animatedProgress.value,
-                    scaleY = 1f * animatedProgress.value,
+                    name = "paw02",
+                    scaleX = 0.94136f * paw02,
+                    scaleY = 1f * paw02,
                     translationX = 50.5f,
                     translationY = 48.335f,
                 ) {
                     Path(
                         pathData = drawParts[2],
-                        stroke = SolidColor(Color.White),
+                        stroke = color,
                         strokeLineWidth = 4f
                     )
                 }
                 Group(
-                    name = "04",
+                    name = "paw03",
                     rotation = -19.37f,
-                    scaleX = -0.94136f * animatedProgress.value,
-                    scaleY = 1f * animatedProgress.value,
+                    scaleX = -0.94136f * paw03,
+                    scaleY = 1f * paw03,
                     translationX = 55.597f,
                     translationY = 48.182f,
                 ) {
                     Path(
                         pathData = drawParts[3],
-                        stroke = SolidColor(Color.White),
+                        stroke = color,
                         strokeLineWidth = 4f
                     )
                 }
                 Group(
-                    name = "05",
+                    name = "paw04",
                     rotation = -25.753f,
-                    scaleX = -0.9197799f * animatedProgress.value,
-                    scaleY = 1f * animatedProgress.value,
+                    scaleX = -0.9197799f * paw04,
+                    scaleY = 1f * paw04,
                     translationX = 59.041f,
                     translationY = 53.054f,
                 ) {
                     Path(
                         pathData = drawParts[4],
-                        stroke = SolidColor(Color.White),
+                        stroke = color,
                         pathFillType = PathFillType.NonZero,
                         strokeLineWidth = 4f,
-                        strokeAlpha = 1f * animatedProgress.value,
+                        strokeAlpha = 1f * paw04,
                         strokeLineCap = StrokeCap.Round,
                         strokeLineJoin = StrokeJoin.Round,
                     )
