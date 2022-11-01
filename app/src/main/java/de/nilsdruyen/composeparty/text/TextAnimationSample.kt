140742f1d942cc3038a8c7f7dc56cf0a5c1005fa
Nils Druyen <nils.druyen@fressnapf.com>
2022-11-01 14:17:27 +0100

    Adjust text animation

--- app/src/main/java/de/nilsdruyen/composeparty/text/TextAnimationSample.kt
+++ app/src/main/java/de/nilsdruyen/composeparty/text/TextAnimationSample.kt
@@ -1,8 +1,7 @@
 package de.nilsdruyen.composeparty.text
 
-import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
@@ -19,6 +18,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.lerp
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 
@@ -29,7 +29,7 @@ fun TextAnimationSample() {
     var animate by remember { mutableStateOf(false) }
     val progress by animateFloatAsState(
         targetValue = if (animate) 0f else 1f,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+        animationSpec = tween()
     )
 
     LaunchedEffect(Unit) {
@@ -47,14 +47,17 @@ fun TextAnimationSample() {
         Text(
             text = "Animated Text",
             modifier = Modifier.align(Alignment.Center),
-            style = lerp(startStyle, endStyle, progress)
+            style = lerp(startStyle, endStyle, progress),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
         )
     }
 }
 
 fun randomTextStyle(): TextStyle = TextStyle(
     color = listOf(Color.Red, Color.Blue, Color.Black, Color.DarkGray, Color.Magenta).random(),
-    fontSize = (30..60).random().sp,
+    fontSize = (20..60).random().sp,
     fontWeight = FontWeight((100..800).random()),
     fontStyle = listOf(FontStyle.Italic, FontStyle.Normal).random(),
+    letterSpacing = (1..15).random().sp,
 )
