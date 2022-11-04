43645ddf8a87d7d1c5b2c04a5be1b3905710d52b
Nils Druyen <nils.druyen@fressnapf.com>
2022-11-04 23:40:06 +0100

    Update ShaderSample

--- app/src/main/java/de/nilsdruyen/composeparty/freestyle/ShaderSample.kt
+++ app/src/main/java/de/nilsdruyen/composeparty/freestyle/ShaderSample.kt
@@ -40,7 +40,7 @@ fun ShaderSample() {
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun ShaderSample(modifier: Modifier = Modifier) {
-    val gradientShader: RuntimeShader = remember { warpSpeedShader() }
+    val gradientShader: RuntimeShader = remember { snowShader() }
     val gradientShaderBrush: ShaderBrush = remember { ShaderBrush(gradientShader) }
 
     SimpleSketchWithCache(speed = 0.08f, modifier = modifier.fillMaxSize()) { time ->
@@ -108,3 +108,54 @@ private fun warpSpeedShader() = RuntimeShader(
         }
     """.trimIndent()
 )
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+private fun snowShader() = RuntimeShader(
+    """
+       uniform float2 iResolution;      // Viewport resolution (pixels)
+       uniform float  iTime;            // Shader playback time (s)
+       
+       vec4 main( in float2 fragCoord )
+       {
+           vec2 uv = fragCoord/iResolution.xy;
+
+           // Time varying pixel color
+           vec3 col = 0.5 + 0.5*cos(iTime*0.4+uv.xyx+vec3(0,2,4));
+
+           // Output to screen
+           return vec4(col,1.0);
+       }
+    """.trimIndent()
+)
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+private fun snow2Shader() = RuntimeShader(
+    """
+       // Source: @kamoshika_vrc https://twitter.com/kamoshika_vrc/status/1495081980278751234
+       uniform float2 iResolution;      // Viewport resolution (pixels)
+       uniform float  iTime;            // Shader playback time (s)
+       
+       const float PI2 = 6.28318530718;
+       float F(vec2 c){
+           return fract(sin(dot(c, vec2(12.9898, 78.233))) * 43758.5453);
+       }
+       
+       half4 main(float2 FC) {
+           vec4 o;
+           float t = iTime;
+           vec2 r = iResolution.xy * vec2(1, -1);
+           vec3 R=normalize(vec3((FC.xy*2.-r)/r.y,1));
+           for(float i=0; i<100; ++i) {
+               float I=floor(t/.1)+i;
+               float d=(I*.1-t)/R.z;
+               vec2 p=d*R.xy+vec2(sin(t+F(I.xx)*PI2)*.3+F(I.xx*.9),t+F(I.xx*.8));
+               if (F(I/100+ceil(p))<.2) {
+                   o+=smoothstep(.1,0.,length(fract(p)-.5))*exp(-d*d*.04);
+               }
+           }
+           return o;
+       }
+    """.trimIndent()
+)
+
+
