ec2e6b2fd9e6f1a22fae0084b13bddbb1a33514c
Nils Druyen <nils.druyen@fressnapf.com>
2022-11-10 21:34:40 +0100

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.0.0-alpha07" apply false
-    id("com.android.library") version "8.0.0-alpha07" apply false
+    id("com.android.application") version "8.0.0-alpha08" apply false
+    id("com.android.library") version "8.0.0-alpha08" apply false
     kotlin("android") version "1.7.20" apply false
     id("com.github.ben-manes.versions") version "0.43.0"
 }
