582f59619fe34189f4c8387e68fbafa596519266
Nils Druyen <nils.druyen@fressnapf.com>
2022-12-05 20:00:36 +0100

    Update agp & accompanist

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.0.0-alpha08" apply false
-    id("com.android.library") version "8.0.0-alpha08" apply false
+    id("com.android.application") version "8.0.0-alpha09" apply false
+    id("com.android.library") version "8.0.0-alpha09" apply false
     kotlin("android") version "1.7.21" apply false
     id("com.github.ben-manes.versions") version "0.44.0"
 }
