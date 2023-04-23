01314f523f53c2e5a5b8f39070b4046050733309
Nils Druyen <info@nilsdruyen.de>
2023-04-23 17:56:37 +0200

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
         implementation(lifecycle)
         implementation(splashscreen)
 
-        implementation(platform("dev.chrisbanes.compose:compose-bom:2023.04.00-alpha02"))
+        implementation(platform("dev.chrisbanes.compose:compose-bom:2023.04.00-alpha03"))
         // Use whichever Compose artifacts you need without a version number
         implementation("androidx.compose.ui:ui")
         implementation("androidx.compose.runtime:runtime")
