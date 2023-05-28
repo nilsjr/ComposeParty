e5f464fbbb7023e52165abc91f081fe9c3229eff
Nils Druyen <nils.druyen@fressnapf.com>
2023-05-28 17:45:32 +0200

    Update to kotlin 1.9.0-Beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.2.0-alpha05" apply false
-    kotlin("android") version "1.8.21" apply false
+    kotlin("android") version "1.9.0-Beta" apply false
     id("com.github.ben-manes.versions") version "0.46.0"
 }
 
