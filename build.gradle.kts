c23f0c2910c0c75d54f30e633977e578015e1286
Nils Druyen <nils.druyen@fressnapf.com>
2023-06-13 07:37:47 +0200

    Updage agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.2.0-alpha06" apply false
+    id("com.android.application") version "8.2.0-alpha07" apply false
     kotlin("android") version "1.9.0-Beta" apply false
-    id("com.github.ben-manes.versions") version "0.46.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 tasks.register<Delete>("clean") {
