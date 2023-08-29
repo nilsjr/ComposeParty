ddafb4c1706708934f921e672cef6e460c0203d3
Nils Druyen <nils.druyen@fressnapf.com>
2023-08-29 12:31:09 +0200

    Update agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.2.0-beta01" apply false
+    id("com.android.application") version "8.3.0-alpha01" apply false
     kotlin("android") version "1.9.10" apply false
     id("com.github.ben-manes.versions") version "0.47.0"
 }
