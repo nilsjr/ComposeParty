eef35adddee70b94688698c450563f326431b68d
Nils Druyen <nils.druyen@fressnapf.com>
2023-09-07 09:57:55 +0200

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
         implementation(lifecycle)
         implementation(splashscreen)
 
-        implementation(platform("dev.chrisbanes.compose:compose-bom:2023.09.00-alpha03"))
+        implementation(platform("dev.chrisbanes.compose:compose-bom:2023.10.00-alpha01"))
         // Use whichever Compose artifacts you need without a version number
         implementation("androidx.compose.ui:ui")
         implementation("androidx.compose.runtime:runtime")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.3.0-alpha01" apply false
+    id("com.android.application") version "8.3.0-alpha02" apply false
     kotlin("android") version "1.9.10" apply false
-    id("com.github.ben-manes.versions") version "0.47.0"
+    id("com.github.ben-manes.versions") version "0.48.0"
 }
 
 tasks.register<Delete>("clean") {
