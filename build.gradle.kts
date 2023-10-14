3af41ed1690b88560bc5bc45b7f6fea9ab46460d
Nils Druyen <info@nilsdruyen.de>
2023-10-14 13:36:18 +0200

    Update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.3.0-alpha07" apply false
-    kotlin("android") version "1.9.20-Beta2" apply false
+    kotlin("android") version "1.9.20-RC" apply false
     id("com.github.ben-manes.versions") version "0.49.0"
 }
 
