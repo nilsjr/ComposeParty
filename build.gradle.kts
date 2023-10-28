ee0cad50d33ae3324769dc749962905e2b641d01
Nils Druyen <info@nilsdruyen.de>
2023-10-28 20:10:52 +0200

    Update kotlin & compose compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.3.0-alpha11" apply false
-    kotlin("android") version "1.9.20-RC" apply false
+    kotlin("android") version "1.9.20-RC2" apply false
     id("com.github.ben-manes.versions") version "0.49.0"
 }
 
