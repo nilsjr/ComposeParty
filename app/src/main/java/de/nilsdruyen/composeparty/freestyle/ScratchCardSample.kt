f96a745d126bea95d6ac2e09ad74462f1001fd38
Nils Druyen <info@nilsdruyen.de>
2023-12-24 23:47:22 +0100

    Fix scratch calculation

--- app/src/main/java/de/nilsdruyen/composeparty/freestyle/ScratchCardSample.kt
+++ app/src/main/java/de/nilsdruyen/composeparty/freestyle/ScratchCardSample.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
@@ -29,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -37,12 +40,16 @@ import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import de.nilsdruyen.composeparty.ui.theme.ComposePartyTheme
 import timber.log.Timber
+import kotlin.math.roundToInt
 
 const val imageUrl = "https://picsum.photos/id/31/1036/2021"
 
@@ -53,26 +60,43 @@ enum class MotionEvent {
 @Composable
 fun ScratchCardSample() {
     var showCanvas by remember { mutableStateOf(true) }
+    var imageSize by remember { mutableStateOf(Size.Zero) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .systemBarsPadding()
-            .background(Color.Black),
+            .background(Color.Black)
+            .onGloballyPositioned {
+                Timber.d("size ${it.size}")
+                if (imageSize == Size.Zero) imageSize = it.size.toSize()
+            },
         contentAlignment = Alignment.Center,
     ) {
-        AsyncImage(model = imageUrl, contentDescription = null)
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f),
+            onState = {
+                if (it is AsyncImagePainter.State.Success) {
+                    Timber.d("size " + it.painter.intrinsicSize)
+                    imageSize = it.painter.intrinsicSize
+                }
+            }
+        )
         AnimatedVisibility(
             visible = showCanvas,
             exit = fadeOut(tween(3000))
         ) {
-            Overlay { showCanvas = false }
+            Overlay(imageSize) { showCanvas = false }
         }
     }
 }
 
 @Composable
-fun Overlay(hideCanvas: () -> Unit) {
+fun Overlay(imageSize: Size, hideCanvas: () -> Unit) {
     val currentView = LocalView.current
     var captureIt by remember { mutableStateOf(false) }
     var capturedBitmap by remember { mutableStateOf<Bitmap?>(null) }
@@ -103,6 +127,7 @@ fun Overlay(hideCanvas: () -> Unit) {
         if (captureIt) {
             captureIt = false
             currentView.capture(
+                imageSize = imageSize,
                 onBitmapReady = { bitmap: Bitmap -> capturedBitmap = bitmap },
                 onBitmapError = {}
             )
@@ -241,14 +266,26 @@ private fun percentTransparent(bm: Bitmap, scale: Int): Float {
     return totalTransparent.toFloat() / (scale * scale)
 }
 
-private fun View.capture(onBitmapReady: (Bitmap) -> Unit, onBitmapError: (Exception) -> Unit) {
+private fun View.capture(
+    imageSize: Size,
+    onBitmapReady: (Bitmap) -> Unit,
+    onBitmapError: (Exception) -> Unit,
+) {
     try {
         val temporalBitmap = Bitmap.createBitmap(this.width, this.height, Bitmap.Config.ARGB_8888)
 
-        val location = IntArray(2)
-        this.getLocationInWindow(location)
-        val viewRectangle =
-            Rect(location[0], location[1], location[0] + this.width, location[1] + this.height)
+        val centerW = this.width / 2
+        val centerH = this.height / 2
+        val left = centerW - (imageSize.width / 2)
+        val right = centerW + (imageSize.width / 2)
+        val top = centerH - (imageSize.height / 2)
+        val bottom = centerH + (imageSize.height / 2)
+
+        val calculatedRectangle =
+            Rect(left.roundToInt(), top.roundToInt(), right.roundToInt(), bottom.roundToInt())
+
+        Timber.d("size calc $calculatedRectangle")
+
         val onPixelCopyListener: PixelCopy.OnPixelCopyFinishedListener =
             PixelCopy.OnPixelCopyFinishedListener { copyResult ->
                 if (copyResult == PixelCopy.SUCCESS) {
@@ -260,7 +297,7 @@ private fun View.capture(onBitmapReady: (Bitmap) -> Unit, onBitmapError: (Except
 
         PixelCopy.request(
             (this.context as Activity).window,
-            viewRectangle,
+            calculatedRectangle,
             temporalBitmap,
             onPixelCopyListener,
             Handler(Looper.getMainLooper())
--- app/src/main/java/de/nilsdruyen/composeparty/freestyle/SnowFallSample.kt
+++ app/src/main/java/de/nilsdruyen/composeparty/freestyle/SnowFallSample.kt
@@ -26,7 +26,11 @@ fun SnowFallSample() {
     ).map {
         it.apply { setBounds(0, 0, intrinsicWidth, intrinsicHeight) }
     }
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .snowfall(snowflakes)
+    ) {
         Image(
             painter = painterResource(id = R.drawable.bg_forest),
             contentDescription = null,
@@ -41,130 +45,6 @@ fun SnowFallSample() {
                         1f to Color(0xE5000000),
                     )
                 )
-                .snowfall(snowflakes)
         )
     }
 }
-
-//internal fun Modifier.snowfall(
-//
-//) = composed {
-//    var snowflakesState by remember {
-//        mutableStateOf(SnowflakesState(-1, IntSize(0, 0)))
-//    }
-//
-//    LaunchedEffect(Unit) {
-//        while (isActive) {
-//            withFrameNanos { newTick ->
-//                val elapsedMillis =
-//                    (newTick - snowflakesState.tickNanos).nanoseconds.inWholeMilliseconds
-//                val wasFirstRun = snowflakesState.tickNanos < 0
-//                snowflakesState.tickNanos = newTick
-//
-//                if (wasFirstRun) return@withFrameNanos
-//                for (snowflake in snowflakesState.snowflakes) {
-//                    snowflake.update(elapsedMillis)
-//                }
-//            }
-//        }
-//    }
-//
-//    onSizeChanged { newSize -> snowflakesState = snowflakesState.resize(newSize) }
-//        .clipToBounds()
-//        .drawWithContent {
-//            drawContent()
-//            snowflakesState.draw(drawContext.canvas)
-//        }
-//}
-//
-//fun ClosedRange<Float>.random() =
-//    ThreadLocalRandom.current().nextFloat() * (endInclusive - start) + start
-//
-//fun Float.random() =
-//    ThreadLocalRandom.current().nextFloat() * this
-//
-//fun Int.random() =
-//    ThreadLocalRandom.current().nextInt(this)
-//
-//fun IntSize.randomPosition() =
-//    Offset(width.random().toFloat(), height.random().toFloat())
-//
-//private const val snowflakeDensity = 0.1
-//private val incrementRange = 0.4f..0.8f
-//private val sizeRange = 5.0f..12.0f
-//private const val angleSeed = 25.0f
-//private val angleSeedRange = -angleSeed..angleSeed
-//private const val angleRange = 0.1f
-//private const val angleDivisor = 10000.0f
-//const val baseFrameDurationMillis = 16
-//const val baseSpeedPxAt60Fps = 3.5f
-//
-//internal data class SnowflakesState(
-//    var tickNanos: Long,
-//    val snowflakes: List<Snowflake>,
-//) {
-//
-//    constructor(tick: Long, canvasSize: IntSize) : this(tick, createSnowflakes(canvasSize))
-//
-//    fun draw(canvas: Canvas) {
-//        snowflakes.forEach { it.draw(canvas) }
-//    }
-//
-//    fun resize(newSize: IntSize) = copy(snowflakes = createSnowflakes(newSize))
-//
-//    companion object {
-//
-//        private fun createSnowflakes(canvasSize: IntSize): List<Snowflake> {
-//            val canvasArea = canvasSize.width * canvasSize.height
-//            val normalizedDensity = snowflakeDensity.coerceIn(0.0..1.0) / 500.0
-//            val snowflakesCount = (canvasArea * normalizedDensity).roundToInt()
-//
-//            return List(snowflakesCount) {
-//                Snowflake(
-//                    incrementFactor = incrementRange.random(),
-//                    size = sizeRange.random(),
-//                    canvasSize = canvasSize,
-//                    position = canvasSize.randomPosition(),
-//                    angle = angleSeed.random() / angleSeed * angleRange + (PI / 2.0) - (angleRange / 2.0)
-//                )
-//            }
-//        }
-//    }
-//}
-//
-//private val snowflakePaint = Paint().apply {
-//    isAntiAlias = true
-//    color = Color.White
-//    style = PaintingStyle.Fill
-//}
-//
-//internal class Snowflake(
-//    private val incrementFactor: Float,
-//    private val size: Float,
-//    private val canvasSize: IntSize,
-//    position: Offset,
-//    angle: Double
-//) {
-//
-//    private var position by mutableStateOf(position)
-//    private var angle by mutableStateOf(angle)
-//
-//    fun update(elapsedMillis: Long) {
-//        val increment =
-//            incrementFactor * (elapsedMillis / baseFrameDurationMillis) * baseSpeedPxAt60Fps
-//
-//        val xDelta = (increment * cos(angle)).toFloat()
-//        val yDelta = (increment * sin(angle)).toFloat()
-//        position = Offset(position.x + xDelta, position.y + yDelta)
-//
-//        angle += angleSeedRange.random() / angleDivisor
-//
-//        if (position.y > canvasSize.height + size) {
-//            position = Offset(position.x, -size)
-//        }
-//    }
-//
-//    fun draw(canvas: Canvas) {
-//        canvas.drawCircle(center = position, radius = size, paint = snowflakePaint)
-//    }
-//}
