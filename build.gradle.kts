23865df02eccc2e04485c7cb713a43dc5d7d8090
Nils Druyen <nils.druyen@fressnapf.com>
2024-01-24 13:25:17 +0100

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.4.0-alpha04" apply false
-    kotlin("android") version "2.0.0-Beta2" apply false
-    id("com.github.ben-manes.versions") version "0.50.0"
+    kotlin("android") version "2.0.0-Beta3" apply false
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 tasks.register<Delete>("clean") {
