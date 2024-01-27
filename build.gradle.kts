728a3e3d91acda9bec55f9254f60b710ae953d28
Nils Druyen <info@nilsdruyen.de>
2024-01-27 19:29:11 +0100

    Update agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.4.0-alpha06" apply false
+    id("com.android.application") version "8.4.0-alpha07" apply false
     kotlin("android") version "2.0.0-Beta3" apply false
     id("com.github.ben-manes.versions") version "0.51.0"
 }
