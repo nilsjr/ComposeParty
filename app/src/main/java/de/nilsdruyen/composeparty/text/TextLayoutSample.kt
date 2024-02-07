20b4f940ab22b657bed6249121dcf73a4c4b431c
Nils Druyen <nils.druyen@fressnapf.com>
2024-02-07 11:27:35 +0100

    Update TextLayoutSample

--- app/src/main/java/de/nilsdruyen/composeparty/text/TextLayoutSample.kt
+++ app/src/main/java/de/nilsdruyen/composeparty/text/TextLayoutSample.kt
@@ -12,60 +12,69 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.tooling.preview.PreviewFontScale
 import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastAny
 import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastMap
+import androidx.compose.ui.util.fastMaxOfOrNull
+import androidx.compose.ui.util.fastSumBy
 import de.nilsdruyen.composeparty.ui.theme.ComposePartyTheme
 
 @Composable
 fun TextLayoutSample(modifier: Modifier = Modifier) {
     var showColumn by remember { mutableStateOf(false) }
 
-    CustomLayout(modifier) {
+    SameSizeElementFlowRow(modifier) {
         Text("Nils addwdwada", modifier = Modifier.background(Color.Red))
         Text("Thomas", modifier = Modifier.background(Color.Blue))
     }
 }
 
 @Composable
-fun CustomLayout(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-    Layout(content = content, modifier) { m, c ->
-        val placeables = mutableListOf<Placeable>()
+fun SameSizeElementFlowRow(
+    modifier: Modifier = Modifier,
+    mainAxisSpacing: Dp = 0.dp,
+    crossAxisSpacing: Dp = 0.dp,
+    content: @Composable () -> Unit
+) {
+    Layout(content = content, modifier = modifier) { measurables, constraints ->
+        val mainAxisSpacingPx = mainAxisSpacing.roundToPx()
+        val crossAxisSpacingPx = crossAxisSpacing.roundToPx()
 
-        val maxW = m.any {
-             it.minIntrinsicWidth(c.minHeight) > c.maxWidth / m.size
+        val totalSpacingPx = mainAxisSpacingPx * (measurables.size - 1)
+        val allowedItemWidth = (constraints.maxWidth - totalSpacingPx) / measurables.size
+
+        val itemExceedsMaxWidth = measurables.fastAny { measurable ->
+            measurable.maxIntrinsicWidth(constraints.minHeight) > allowedItemWidth
         }
 
-        val constraint = if (maxW) {
-            Constraints(minWidth = c.maxWidth, maxWidth = c.maxWidth)
+        val placeables = measurables.fastMap { measurable ->
+            val constraint = when {
+                itemExceedsMaxWidth -> Constraints.fixedWidth(constraints.maxWidth)
+                else -> Constraints.fixedWidth(allowedItemWidth)
+            }
+            measurable.measure(constraint)
+        }
+
+        val width = constraints.maxWidth
+        val height = if (itemExceedsMaxWidth) {
+            placeables.fastSumBy { it.measuredHeight } + (measurables.size - 1) * crossAxisSpacingPx
         } else {
-            Constraints(minWidth = c.maxWidth / 2, maxWidth = c.maxWidth)
-        }
-
-        m.fastForEach { measurable ->
-            val placeable = measurable.measure(constraint)
-            placeables.add(placeable)
-        }
-
-        val width = c.maxWidth
-        val height = if (maxW) {
-            placeables.sumOf { it.measuredHeight }
-        } else {
-            placeables.first().measuredHeight
+            placeables.fastMaxOfOrNull { it.measuredHeight } ?: 0
         }
 
         var x = 0
         var y = 0
         layout(width, height) {
-            placeables.fastForEach { p ->
-                p.placeRelative(x, y)
-                if (!maxW) {
-                    x += p.measuredWidth
-                }
-                if (maxW) {
-                    y += p.measuredHeight
+            placeables.fastForEach { placeable ->
+                placeable.placeRelative(x, y)
+                if (!itemExceedsMaxWidth) {
+                    x += placeable.width + mainAxisSpacingPx
+                } else {
+                    y += placeable.height + crossAxisSpacingPx
                 }
             }
         }
