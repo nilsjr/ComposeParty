b82cc532603bd7d8d618982332df184367bcd5ab
Nils Druyen <nils.druyen@fressnapf.com>
2024-05-22 12:09:52 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application") version "8.6.0-alpha02" apply false
-    kotlin("android") version "2.0.0-RC3" apply false
-    id("com.github.ben-manes.versions") version "0.51.0"
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.androidGradle) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ben.manes.versions) apply false
 }
 
 tasks.register<Delete>("clean") {
