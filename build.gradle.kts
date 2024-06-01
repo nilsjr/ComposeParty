15b5e5525edf37fdcf4af1cab14527fdd0b9e4ca
Nils Druyen <info@nilsdruyen.de>
2024-06-01 07:15:52 +0200

    Update deps & gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.androidGradle) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ben.manes.versions) apply false
+    alias(libs.plugins.ben.manes.versions)
 }
 
 tasks.register<Delete>("clean") {
