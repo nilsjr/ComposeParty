ff63ff960efcaac818e8ebedc3a2724c6b7b454f
Nils Druyen <info@nilsdruyen.de>
2024-09-02 17:48:00 +0200

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "de.nilsdruyen.composeparty"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "de.nilsdruyen.composeparty"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
