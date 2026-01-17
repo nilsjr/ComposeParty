7752bd9658132bfb22b41027463cb5a1ebeac019
Nils Druyen <info@nilsdruyen.de>
2026-01-17 13:10:34 +0100

    Update Gradle and dependencies, configure daemon JVM toolchain
    - Add `gradle-daemon-jvm.properties` to configure the Gradle daemon to use Java 25.
    - Update Gradle wrapper to version 9.3.0.
    - Update Android Gradle Plugin to 9.1.0-alpha05 and Compose BOM to 2026.01.00.
    - Override Kotlin Gradle plugin version to 2.3.0 in `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
     alias(libs.plugins.ben.manes.versions)
 }
 
+buildscript {
+    dependencies {
+        // override kotlin gradle plugin version
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(layout.buildDirectory)
 }
