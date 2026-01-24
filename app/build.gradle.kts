51a7094e46ea4fb440aba41776eec8827a383f0e
Nils Druyen <info@nilsdruyen.de>
2026-01-24 13:55:44 +0100

    Update build configuration and dependencies
    - Change `androidx.compose.ui.tooling.preview` from `debugImplementation` to `implementation`.
    - Remove `android.useAndroidX=true` from `gradle.properties`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.tooling.preview)
 
     implementation(libs.accompanist.placeholder.material)
 
